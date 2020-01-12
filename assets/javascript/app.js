281d47de428fa74f2f30f83157cdc68d7602d338
Lynde Capogreco <lyndecapc@gmail.com>
2020-01-12 07:46:55 -0600

    setting up files and pseudocaoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function) () {
+
+    //GLOBAL VARIABLES and ARRAYS
+    //variables
+    //questions and answer arrays
+
+    //FUNCTIONS
+
+
+    //Timer start countdown stop
+    //Randomly choose question from array
+    //click function to choose answer
+
+    //keeping score score screen?
+
+
+    //game reset
+}
