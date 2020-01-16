26e1fb4816d47ea7818477a5218e6bc19fb47c70
Lynde Capogreco <lyndecapc@gmail.com>
2020-01-16 12:09:05 -0600

    added user story

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
+//USER STORY: page loads, user sees welcome message or instructions and start button. 
+//user clicks start button, welcome screen/instructions go away, and they see a timer that is counting down. They also see 1st question and answer choices
+//if user chooses answer: message displayed indicates if question was correct or incorrect. if incorrect, correct answer is displayed.
+//after 3 seconds, the next question is displayed
+//if time runs out before user picks an answer, the correct answer is shown
+//after 3 seconds, the next question is displayed
+//once all questions/correct answers have been displayed, a results screen appears with number of correct, incorrect, and unanswered questions
+//also will see a restart button
+//if they click the restart button, the quiz starts over
+//HTML PLANNING: 
+    //Three main sections: Welcome screen and start button, questions div with place for timer, the question text and the answers (dynamic or hard coded)
+    //results div with place for correct, incorrect, and unanswered questions stats AND restart button
+    //CREATE: basic HTML
+//PSEUDOCODE
+
 $(document).ready(function) () {
 
     //GLOBAL VARIABLES and ARRAYS
@@ -5,10 +20,10 @@ $(document).ready(function) () {
     //questions and answer arrays
 
     //FUNCTIONS
-
+    //Randomly choose question from array
 
     //Timer start countdown stop
-    //Randomly choose question from array
+   
     //click function to choose answer
 
     //keeping score score screen?
